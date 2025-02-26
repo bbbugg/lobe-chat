1e5d7e83baf5b35f6e384cccdc42a4ee81b5627b
bbbugg <80089841+bbbugg@users.noreply.github.com>
2025-02-26 10:26:25 +0800

    Update branding.ts

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -5,7 +5,7 @@
 export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
 
 export const BRANDING_NAME = 'Mithrandir';
-export const BRANDING_LOGO_URL = 'https://s3-for-lobechat.laimerx.cn/%E5%8F%AF%E7%88%B1%E5%B0%8F%E7%8C%AB.svg';
+export const BRANDING_LOGO_URL = 'https://s3.laimex.cn/cutecat.svg';
 
 export const ORG_NAME = 'LobeHub';
 

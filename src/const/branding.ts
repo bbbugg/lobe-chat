94e648bab980210b2ff0e4b399c9cc9d0f01aa04
bbbugg <daming20120101@163.com>
2025-03-06 13:00:58 +0800

    test: 使用默认logo

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -5,7 +5,7 @@
 export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
 
 export const BRANDING_NAME = 'Mithrandir';
-export const BRANDING_LOGO_URL = '/icons/cutecat.svg';
+export const BRANDING_LOGO_URL = '';
 
 export const ORG_NAME = 'LobeHub';
 

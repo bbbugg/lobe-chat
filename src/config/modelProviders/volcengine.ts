02313ea04e3ab401edc33daabcfe6244a08f6d11
lobehubbot <i@lobehub.com>
2025-05-25 09:48:53 +0000

    📝 docs(bot): Auto sync agents & plugin to readme

--- src/config/modelProviders/volcengine.ts
+++ src/config/modelProviders/volcengine.ts
@@ -5,8 +5,8 @@ const Doubao: ModelProviderCard = {
   chatModels: [],
   description:
     '字节跳动推出的大模型服务的开发平台，提供功能丰富、安全以及具备价格竞争力的模型调用服务，同时提供模型数据、精调、推理、评测等端到端功能，全方位保障您的 AI 应用开发落地。',
-  id: 'volcengine',
   enabled: true,
+  id: 'volcengine',
   modelsUrl: 'https://www.volcengine.com/docs/82379/1330310',
   name: 'Volcengine',
   settings: {

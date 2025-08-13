ed1a108a7e1d0ffda05f17b6fb8b6e508e68062d
bbbugg <daming20120101@163.com>
2025-08-13 21:15:03 +0800

    修复bug

---
+++ src/app/public/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next';
+import React from 'react';
+
+// 你可以在这里定义整个网站的默认元数据（比如标题、描述），对SEO友好
+export const metadata: Metadata = {
+  title: '', // 可以改成你的网站名
+  description: '',
+};
+
+// 这就是根布局组件
+export default function RootLayout({
+                                     children,
+                                   }: {
+  children: React.ReactNode; // 'children' 是一个特殊的 prop，它代表被包裹的子组件（在这里就是你的 page.tsx）
+}) {
+  return (
+    <html lang="en">
+    <body>
+    {/*
+          你的所有页面内容 (page.tsx) 都会被渲染到这里
+          所以 {children} 是必须的
+        */}
+    {children}
+    </body>
+    </html>
+  );
+}

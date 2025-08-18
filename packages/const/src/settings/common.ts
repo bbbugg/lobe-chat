69312e279449594938463fe3d67a248dcac184f7
bbbugg <daming20120101@163.com>
2025-08-18 22:16:42 +0800

    更新默认动画模式为 disabled。

--- packages/const/src/settings/common.ts
+++ packages/const/src/settings/common.ts
@@ -1,7 +1,7 @@
 import { UserGeneralConfig } from '@/types/user/settings';
 
 export const DEFAULT_COMMON_SETTINGS: UserGeneralConfig = {
-  animationMode: 'agile',
+  animationMode: 'disabled',
   fontSize: 14,
   highlighterTheme: 'lobe-theme',
   mermaidTheme: 'lobe-theme',

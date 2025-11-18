ad3862eb07bd2c2b5fe228414611106a99462b77
bbbugg <daming20120101@163.com>
2025-11-18 16:38:36 +0800

    Reapply "✨ feat: adjust compression level for batch download zip files"
    This reverts commit 8eeb92628ef2e5e9f3beec70da3320b155eccbaf.

--- src/app/(backend)/webapi/files/download/route.ts
+++ src/app/(backend)/webapi/files/download/route.ts
@@ -37,7 +37,7 @@ export const POST = async (req: NextRequest) => {
     }
 
     const archive = archiver('zip', {
-      zlib: { level: 1 },
+      zlib: { level: 0 },
     });
 
     const fileName = `${BRANDING_NAME}_batch_download_${getYYYYmmddHHMMss(new Date())}.zip`;
